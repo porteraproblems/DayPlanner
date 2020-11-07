201ba50aacf515cf7a06472ebecbadb9ebcbe95b
Alexander Portera <aportera3@gmail.com>
2020-11-07 15:12:32 -0500

    changed more things around in the js and html

--- planner.js
+++ planner.js
@@ -3,16 +3,18 @@ function compareTime() {
     var nowTime = parseInt(moment().format('HH'));
     //Start from 9AM, till 5PM
     for (time = 9; time >= 17; time++) {
-        var timeBlock = parseInt($("#" + time + "hr").attr("data-index"));
-        console.log(timeBlock);
-        if (timeBlock < nowTime) {
-            $("#" + time + "hr").addClass("past");
-        } else if (timeBlock == nowTime) {
-            $("#" + time + "hr").addClass("present");
-        } else if (timeBlock > nowTime) {
-            $("#" + time + "hr").addClass("future");
+        var description = parseInt($("." + time + "hr").attr("data-index"));
+        console.log(description);
+        if (description < nowTime) {
+            $("." + time + "hr").addClass("past");
+        } else if (description == nowTime) {
+            $("." + time + "hr").addClass("present");
+        } else if (description > nowTime) {
+            $("." + time + "hr").addClass("future");
         }
     }
 }
 
 //function with event listener for the save button
+var date = new Date()
+$("#currentDay").text(date.toDateString())
