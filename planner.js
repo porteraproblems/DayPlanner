553520063da339c34b0f17ddb33e80f333d3c093
Alexander Portera <aportera3@gmail.com>
2020-11-07 15:45:27 -0500

    added event listener to save button, made local storage work, and added the rest of the time slots

--- planner.js
+++ planner.js
@@ -3,7 +3,7 @@ function compareTime() {
     var nowTime = parseInt(moment().format('HH'));
     //Start from 9AM, till 5PM
     for (time = 9; time >= 17; time++) {
-        var description = parseInt($("." + time + "hr").attr("data-index"));
+        var description = parseInt($("." + time + "hr").attr("data-i"));
         console.log(description);
         if (description < nowTime) {
             $("." + time + "hr").addClass("past");
@@ -15,6 +15,30 @@ function compareTime() {
     }
 }
 
-//function with event listener for the save button
+// timestamp
 var date = new Date()
 $("#currentDay").text(date.toDateString())
+
+//function with event listener for the save button
+function saveData(event) {
+    event.preventDefault();
+
+    var value = $(this).prev().val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, value);
+
+};
+
+$(".saveBtn").click(saveData);
+
+for (let i = 0; i < localStorage.length; i++) {
+    var key = localStorage.key(i);
+    var value = localStorage.getItem(key);
+    
+    $(".description").each(function() {
+        if ($(this).attr("id") == key) {
+            $(this).text(value);
+        }
+    });
+}
